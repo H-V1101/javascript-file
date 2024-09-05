2d506f60e5efffe6f8b2e1f01182385b4284e2be
H-V1011 <hyvardhan1234@gmail.com>
2024-09-05 06:27:38 +0530

    javascript

---
+++ 00_intro_to_js/index.js
@@ -0,0 +1,7 @@
+// javascript is an programming langugae . to run javascript outside the browser we use nodejs.
+
+// this is just an introduction part of the series 
+
+// to print the thing 
+
+console.log(98);
