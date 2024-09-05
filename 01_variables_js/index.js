e9bed5f563393fdbd4ccff48cfe7e40ebe9dc9e7
H-V1011 <hyvardhan1234@gmail.com>
2024-09-05 07:15:56 +0530

    variables

---
+++ 01_variables_js/index.js
@@ -0,0 +1,10 @@
+//  variables are the containers that store values . 
+// it contains datatypes , strings etc 
+ 
+//  In js we change things in runtime it is an dynamic language 
+console.log("In this we are learning to declare variables etc")
+// we can use var , let , const to declare variables
+let a=69;
+console.log(a);
+a="this is the value of 69"
+console.log(a)
