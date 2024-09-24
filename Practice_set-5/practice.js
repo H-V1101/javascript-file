6a5322bad8bc040f00e4496d66cbab9b09804e32
H-V1011 <hyvardhan1234@gmail.com>
2024-09-24 09:42:07 +0530

    practice_set-5

--- Practice_set-5/practice.js
+++ Practice_set-5/practice.js
@@ -1,30 +1,42 @@
 //Create an array of number and take input from user to add the number of this array
-// const prompt =require("prompt-sync")();
-// let a =prompt("write ur number:");
-// a=Number.parseInt(a);
-// let arr=[67,87,98,89,23,76];
-// a=Number.parseInt(a);4
-// arr.push(a);
-// console.log(arr);
+const prompt =require("prompt-sync")();
+let a =prompt("write ur number:");
+a=Number.parseInt(a);
+let arr=[67,87,98,89,23,76];
+a=Number.parseInt(a);4
+arr.push(a);
+console.log(arr);
 
 
 //print an 0 in array repeat the process unteil 0 is printed in it.
-// const prompt= require("prompt-sync")();
-// let arr = [12,43,22,11,3,89,87];
-// let a;
-// do{
-//  a = prompt("Enter a Number");
-// a = Number.parseInt(a);
-// arr.push(a);
-// }while(a != 0);
-// console.log(arr);
+const prompt= require("prompt-sync")();
+let arr = [12,43,22,11,3,89,87];
+let a;
+do{
+ a = prompt("Enter a Number");
+a = Number.parseInt(a);
+arr.push(a);
+}while(a != 0);
+console.log(arr);
 
 // filter the number divisible by 10 from a given array.
-// const prompt=require("prompt-sync")();
-// a = prompt("Enter a Number");
-// a=Number.parseInt(a);
+const prompt=require("prompt-sync")();
+a = prompt("Enter a Number");
+a=Number.parseInt(a);
 let arr= [10,29,20,30,40,50,87,89];
 let a =arr.filter((a)=>{
     return a % 10==0;
 })
 console.log(a);
+
+
+
+//create an array of square of given number .
+let arr = [2,3,4,5,6,7,8,9];
+let a = arr.map((value,index,array)=>{
+//  console.log(value,index,array);
+ return value*value;
+})
+console.log(a);
+
+
--- Practice_set-5/tempCodeRunnerFile.js
+++ Practice_set-5/tempCodeRunnerFile.js
@@ -1,3 +1,7 @@
-const prompt=require("prompt-sync")();
-// a = prompt("Enter a Number");
-// a=Number.parseInt(a);
+const prompt =require("prompt-sync")();
+let a =prompt("write ur number:");
+a=Number.parseInt(a);
+let arr=[67,87,98,89,23,76];
+a=Number.parseInt(a);4
+arr.push(a);
+console.log(arr);
